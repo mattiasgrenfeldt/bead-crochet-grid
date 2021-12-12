f03eb0995a3dade29c9b286b041df9757063899c
mattiasgrenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2021-12-12 16:29:09 +0100

    PDF generation using latex

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bead-crochet-grid
+
+Make sure to have `pdflatex` installed. Then just run:
+
+```sh
+./gen.sh
+```
+
+You can adjust the image settings in `main.py` and the number of pages in `grid.tex`.
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from PIL import Image, ImageDraw
 
-WIDTH = 1000
-HEIGHT = 1000
-CELL_SIZE = 10
+WIDTH = 2100 # a4 paper dimensions 
+HEIGHT = 2970
+CELL_SIZE = 25
 BACKGROUND_COLOR = (255, 255, 255)
-GRID_COLOR = (0, 0, 0)
-GRID_WIDTH = 1
+GRID_LINE_COLOR = (0, 0, 0)
+GRID_LINE_WIDTH = 1
 
 img = Image.new("RGB", (WIDTH, HEIGHT), BACKGROUND_COLOR)
 draw = ImageDraw.Draw(img)
@@ -15,7 +15,7 @@ draw = ImageDraw.Draw(img)
 for y in range(0, HEIGHT, CELL_SIZE):
     p0 = (0, y)
     p1 = (WIDTH - 1, y)
-    draw.line((p0, p1), fill=GRID_COLOR, width=GRID_WIDTH)
+    draw.line((p0, p1), fill=GRID_LINE_COLOR, width=GRID_LINE_WIDTH)
 
 # vertical lines
 for (i, y) in enumerate(range(0, HEIGHT, CELL_SIZE)):
@@ -23,6 +23,6 @@ for (i, y) in enumerate(range(0, HEIGHT, CELL_SIZE)):
     for x in range(offset, WIDTH, CELL_SIZE):
         p0 = (x, y)
         p1 = (x, y + CELL_SIZE)
-        draw.line((p0, p1), fill=GRID_COLOR, width=GRID_WIDTH)
+        draw.line((p0, p1), fill=GRID_LINE_COLOR, width=GRID_LINE_WIDTH)
 
 img.save("grid.png")
